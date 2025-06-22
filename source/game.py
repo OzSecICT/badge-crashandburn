e1b9241ab18df31d830dbf38fab0c92f8b4c33d5
Jason Taylor <jetaylor@gmail.com>
2025-06-22 18:18:03 -0500

    Add debug and cleanup game.py

--- source/game.py
+++ source/game.py
@@ -2,30 +2,59 @@ import button
 import led
 
 class Game:
-    """ Base game class."""
+    """
+    Base game class
+    """
     def __init__(self):
+        print("Initializing base Game class")
         self.__running = False
 
     def set_running(self, running):
+        print(f"Setting game running state to {running}")
         self.__running = running
 
     def register_callbacks(self):
-        """ Register button callbacks. Requires overriding"""
+        """
+        Register button callbacks
+        Overload with your own logic.
+        """
+        print("Registering default button callbacks to do_nothing")
+        button.up.callback = self.do_nothing  # type: ignore
+        button.down.callback = self.do_nothing  # type: ignore
+        button.left.callback = self.do_nothing  # type: ignore
+        button.right.callback = self.do_nothing  # type: ignore
+        button.a.callback = self.do_nothing  # type: ignore
+        button.b.callback = self.do_nothing  # type: ignore
+        button.start.callback = self.do_nothing  # type: ignore
+        button.select.callback = self.do_nothing  # type: ignore
+
+    def do_nothing(self):
+        """
+        Does nothing
+        Useful for buttons that are not used in the game.
+        """
+        print("Button pressed, doing nothing.")
         pass
 
     @property
     def is_running(self):
+        print(f"Game state is: {self.__running}")
         return self.__running
     
     def run(self):
-        """ Set up the board for the game. Requires overriding"""
+        """
+        Set up the board for the game
+        Overload with your own logic.
+        """
         self.set_running(True)
         led.clear()
         button.clear()
         self.register_callbacks()
 
     def exit(self):
-        """ Exit the game. """
+        """
+        Exit the game and set running state to False
+        """
         led.clear()
         button.clear()
         self.set_running(False)
